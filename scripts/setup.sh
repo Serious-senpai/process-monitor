0cc20613260068896feb5bc50e5d05e4ffb46ee6
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-11-06 05:29:11 -0500

    Setup virtual machine

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+apt-get update
+apt-get install -y build-essential cmake curl doxygen git graphviz libssl-dev openssh-server pkg-config python3 python-is-python3
+
+# Install Rust toolchain
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=1.91 -y
+rustup toolchain install nightly
+rustup component add rust-src
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+cargo install bpf-linker cargo-generate
+
+# Start OpenSSH server
+systemctl enable ssh
+systemctl start ssh
