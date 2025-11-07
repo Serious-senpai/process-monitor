6bebfe77076fec870c8bc25637cdc7fd7167f65c
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-11-07 14:11:12 +0700

    Add missing sudo to setup commands

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-apt-get update
-apt-get install -y build-essential cmake curl doxygen git graphviz libssl-dev openssh-server pkg-config python3 python-is-python3
+sudo apt-get update
+sudo apt-get install -y build-essential cmake curl doxygen git graphviz libssl-dev openssh-server pkg-config python3 python-is-python3
 
 # Install Rust toolchain
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=1.91 -y
@@ -11,5 +11,5 @@ rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 cargo install bpf-linker cargo-generate
 
 # Start OpenSSH server
-systemctl enable ssh
-systemctl start ssh
+sudo systemctl enable ssh
+sudo systemctl start ssh
