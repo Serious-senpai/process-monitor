95de45db8f7b32ed18d537f2d455a32551afa990
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-11-07 06:09:21 -0500

    Setup XDP and KProbe hooks

--- linux-listener/linux-listener/src/main.rs
+++ linux-listener/linux-listener/src/main.rs
@@ -1,55 +1,96 @@
-use anyhow::Context;
-use aya::programs::{Xdp, XdpFlags};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread;
+use std::time::Duration;
+
+use aya::Ebpf;
+use aya::programs::{KProbe, Xdp};
 use aya_log::EbpfLogger;
 use clap::Parser;
+use log::LevelFilter;
 use log::{info, warn};
+use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode};
 use tokio::signal;
 
 #[derive(Debug, Parser)]
-struct Opt {
+struct Arguments {
     #[clap(short, long, default_value = "eth0")]
-    iface: String,
+    pub interface: String,
+}
+
+fn initialize_logger(level: LevelFilter) -> Box<TermLogger> {
+    let cfg = ConfigBuilder::new()
+        .set_location_level(level)
+        .set_time_offset_to_local()
+        .unwrap_or_else(|e| e)
+        .build();
+
+    TermLogger::new(level, cfg, TerminalMode::Stderr, ColorChoice::Auto)
+}
+
+fn attach_recv(hook: &mut KProbe) -> anyhow::Result<()> {
+    hook.load()?;
+    hook.attach("__sys_recvmsg", 0)?;
+    hook.attach("__sys_recvfrom", 0)?;
+    Ok(())
+}
+
+fn attach_xdp(hook: &mut Xdp, interface: &str) -> anyhow::Result<()> {
+    hook.load()?;
+    hook.attach(interface, aya::programs::XdpFlags::default())?;
+    Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    let opt = Opt::parse();
+async fn main() -> anyhow::Result<()> {
+    let arguments = Arguments::parse();
+    let _ = log::set_boxed_logger(initialize_logger(LevelFilter::Info))
+        .inspect_err(|e| warn!("Unable to set default logger: {e}"));
 
-    env_logger::init();
-
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Ebpf::load_file` instead.
-    let mut bpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+    let mut ebpf = Ebpf::load(aya::include_bytes_aligned!(concat!(
         env!("OUT_DIR"),
         "/linux-listener"
     )))?;
-    match EbpfLogger::init(&mut bpf) {
-        Err(e) => {
-            // This can happen if you remove all log statements from your eBPF program.
-            warn!("failed to initialize eBPF logger: {e}");
-        }
-        Ok(logger) => {
-            let mut logger =
-                tokio::io::unix::AsyncFd::with_interest(logger, tokio::io::Interest::READABLE)?;
-            tokio::task::spawn(async move {
-                loop {
-                    let mut guard = logger.readable_mut().await.unwrap();
-                    guard.get_inner_mut().flush();
-                    guard.clear_ready();
-                }
-            });
-        }
-    }
-    let program: &mut Xdp = bpf.program_mut("xdp_firewall").unwrap().try_into()?;
-    program.load()?;
-    program.attach(&opt.iface, XdpFlags::default())
-        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
-    info!("Waiting for Ctrl-C...");
-    signal::ctrl_c().await?;
-    info!("Exiting...");
+    let stopped = Arc::new(AtomicBool::new(false));
+    let handle = match EbpfLogger::init(&mut ebpf) {
+        Ok(mut logger) => {
+            info!("eBPF logger initialized");
+
+            let stopped = stopped.clone();
+            Some(thread::spawn(move || {
+                while !stopped.load(Ordering::Relaxed) {
+                    logger.flush();
+                    thread::sleep(Duration::from_secs(1));
+                }
+            }))
+        }
+        Err(e) => {
+            warn!("Failed to init eBPF logger: {e}");
+            None
+        }
+    };
+
+    attach_recv(
+        ebpf.program_mut("recv_variants_hook")
+            .expect("Check the eBPF program again")
+            .try_into()?,
+    )?;
+    attach_xdp(
+        ebpf.program_mut("xdp_hook")
+            .expect("Check the eBPF program again")
+            .try_into()?,
+        &arguments.interface,
+    )?;
+
+    info!("Listening for packets... Press Ctrl+C to exit.");
+    signal::ctrl_c().await.expect("Unable to listen for Ctrl-C");
+
+    info!("Received Ctrl-C. Exiting...");
+    stopped.store(true, Ordering::Relaxed);
+    if let Some(handle) = handle {
+        handle.join().unwrap();
+    }
 
     Ok(())
 }
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,13 +3,13 @@
 sudo apt-get update
 sudo apt-get install -y build-essential cmake curl doxygen git graphviz libssl-dev openssh-server pkg-config python3 python-is-python3
 
-# Install Rust toolchain
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=1.91 -y
-rustup toolchain install nightly
-rustup component add rust-src
-rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
-cargo install bpf-linker cargo-generate
-
 # Start OpenSSH server
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
+# Install Rust toolchain
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=1.91 -y
+$HOME/.cargo/bin/rustup toolchain install nightly
+$HOME/.cargo/bin/rustup component add rust-src
+$HOME/.cargo/bin/rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+$HOME/.cargo/bin/cargo install bpf-linker cargo-generate
