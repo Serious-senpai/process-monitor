2c2ab4ef63c3029543bc6bc07b6e592354d5e920
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2026-02-09 15:28:17 +0700

    Complete IRP major function hook

--- windows-listener/irp-hook/irp-hook/driver.c
+++ windows-listener/irp-hook/irp-hook/driver.c
@@ -1,5 +1,6 @@
 #include <wdm.h>
 
+PDRIVER_DISPATCH original_irp_handlers[IRP_MJ_MAXIMUM_FUNCTION + 1] = {NULL};
 PDRIVER_OBJECT target_driver = NULL;
 PFILE_OBJECT target_file = NULL;
 
@@ -7,11 +8,31 @@ static void driver_unload(PDRIVER_OBJECT driver)
 {
     UNREFERENCED_PARAMETER(driver);
 
+    if (target_driver != NULL)
+    {
+        for (UCHAR i = 0; i < IRP_MJ_MAXIMUM_FUNCTION + 1; i++)
+        {
+            target_driver->MajorFunction[i] = original_irp_handlers[i];
+        }
+        target_driver = NULL;
+    }
+
     if (target_file != NULL)
     {
         ObDereferenceObject(target_file);
         target_file = NULL;
     }
+}
+
+NTSTATUS hooked_irp_handler(PDEVICE_OBJECT device, PIRP irp)
+{
+    DbgPrint("hooked_irp_handler called\n");
+
+    IO_STACK_LOCATION *stack = IoGetCurrentIrpStackLocation(irp);
+    UCHAR major = stack->MajorFunction;
+    DbgPrint("MajorFunction: %d\n", major);
+
+    return original_irp_handlers[major](device, irp);
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING registry)
@@ -25,15 +46,24 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING registry)
         &target_device_name,
         FILE_READ_DATA,
         &target_file,
-        &target_device
-    );
+        &target_device);
 
-    if (!NT_SUCCESS(status)) {
+    if (!NT_SUCCESS(status))
+    {
         DbgPrint("IoGetDeviceObjectPointer failed: 0x%X\n", status);
         driver_unload(driver);
         return status;
     }
 
     target_driver = target_device->DriverObject;
+    for (UCHAR i = 0; i < IRP_MJ_MAXIMUM_FUNCTION + 1; i++)
+    {
+        original_irp_handlers[i] = target_driver->MajorFunction[i];
+        if (target_driver->MajorFunction[i] != NULL)
+        {
+            target_driver->MajorFunction[i] = hooked_irp_handler;
+        }
+    }
+
     return STATUS_SUCCESS;
 }
